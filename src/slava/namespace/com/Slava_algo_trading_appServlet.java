9801a394f9bb14e8333f1e4e073569dcf172a3ec
slavaroi <slava.roi@gmail.com>
2014-12-23 23:54:13 +0200

    Exercise 2

--- src/slava/namespace/com/Slava_algo_trading_appServlet.java
+++ src/slava/namespace/com/Slava_algo_trading_appServlet.java
@@ -1,11 +1,17 @@
 package slava.namespace.com;
+
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class Slava_algo_trading_appServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	
+	int num1 = 4, num2 = 3, num3 = 7;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Result of (" + num1 + " + " + num2 + ") * "+ num3 + " = 49");
 	}
+
 }
